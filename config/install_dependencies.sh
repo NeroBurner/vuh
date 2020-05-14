879f28dade822fde051a2a2a800afa06361a4f7d
Reinhold Gschweicher <pyro4hell@gmail.com>
2020-05-14 21:55:26 +0200

    [WIP] use NeroBurner forks for Vulkan install targets

--- config/install_dependencies.sh
+++ config/install_dependencies.sh
@@ -6,10 +6,10 @@ cget init --std=c++14
 
 # cget install KhronosGroup/Vulkan-Headers
 
-git clone --branch sdk-1.1.82.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Headers.git ${CGET_PREFIX}/src/vulkan-headers
+git clone --branch cmake_install --depth 1 --recurse-submodules https://github.com/NeroBurner/Vulkan-Headers.git ${CGET_PREFIX}/src/vulkan-headers
 cget install ${CGET_PREFIX}/src/vulkan-headers
 
-git clone --branch sdk-1.1.82.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Loader ${CGET_PREFIX}/src/vulkan-loader
+git clone --branch cmake_export_target --depth 1 --recurse-submodules https://github.com/NeroBurner/Vulkan-Loader ${CGET_PREFIX}/src/vulkan-loader
 cget install ${CGET_PREFIX}/src/vulkan-loader -DBUILD_WSI_WAYLAND_SUPPORT=OFF -DBUILD_TESTS=OFF
 
 git clone --branch 7.8.2853 --depth 1 --recurse-submodules https://github.com/KhronosGroup/glslang.git ${CGET_PREFIX}/src/glslang
